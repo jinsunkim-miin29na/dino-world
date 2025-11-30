bb3e6a6ad6ec66049690fd1ea90a78a7861847ed
jinsunkim-miin29na <53202481+jinsunkim-miin29na@users.noreply.github.com>
2025-11-30 18:38:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -184,3 +184,7 @@ function extractId(url) {
 let tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 document.body.appendChild(tag);
+
+function closeSettings() {
+    goHome();
+}
